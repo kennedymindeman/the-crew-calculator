6a1cbb48d92c681ccf29641d72d6e3912ecdeec9
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-22 22:11:13 -0500

    Chagne possilbe_cards to certain_cards

--- test/test_player_view.py
+++ test/test_player_view.py
@@ -9,4 +9,4 @@ def test_everything_unknown_before_draw():
 
 def test_no_cards_known_before_draw():
     player_view = PlayerView()
-    assert player_view.possible_cards == set()
+    assert player_view.certain_cards == set()
