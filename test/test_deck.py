87849fc0b7cb49292e32213995ee66019801f1f3
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-22 21:29:53 -0500

    Test that shuffling cards results in unique deck

--- test/test_deck.py
+++ test/test_deck.py
@@ -1,3 +1,4 @@
+import random
 from src.card import Card, Suit
 from src.deck import Deck
 
@@ -9,3 +10,11 @@ def test_blue_1_in_deck():
 def test_all_cards_in_deck_stack_after_constructor():
     deck = Deck()
     assert set(deck.draw_pile) == Deck.ALL_CARDS
+
+
+def test_deck_shuffle_results_in_unique_deck():
+    random.seed(0)
+    decks = (Deck(), Deck())
+    for deck in decks:
+        deck.shuffle()
+    assert decks[0].draw_pile != decks[1].draw_pile
