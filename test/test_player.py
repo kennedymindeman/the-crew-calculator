2a229d8f786d9ae3f1d9888203c1b199c555d8d8
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-22 22:32:41 -0500

    Add test file for Player class

---
+++ test/test_player.py
@@ -0,0 +1,4 @@
+def test_player_has_player_view_equal_to_number_of_players():
+    num_players = 4
+    player = Player(player_id=0, num_players=num_players)
+    assert len(player.player_views) == num_players
