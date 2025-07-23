81247b48f6836a30d0d6b371c35b0ac244c9c9db
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-22 21:17:18 -0500

    Add check for proper rank after constructor

--- test/test_card.py
+++ test/test_card.py
@@ -4,3 +4,8 @@ from src.card import Card, Suit
 def test_card_suit_matches_constructor():
     card = Card(Suit.BLUE, 9)
     assert card.suit == Suit.BLUE
+
+
+def test_card_rank_matches_constructor():
+    card = Card(Suit.BLUE, 9)
+    assert card.rank == 9
