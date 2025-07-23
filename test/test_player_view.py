ee72404e6e94d661bfeb2e3f1c4c1b58484fb62b
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-22 22:14:38 -0500

    Add test for mark_has_card removing card from possible cards

--- test/test_player_view.py
+++ test/test_player_view.py
@@ -1,3 +1,4 @@
+from src.card import Card, Suit
 from src.deck import Deck
 from src.player_view import PlayerView
 
@@ -10,3 +11,10 @@ def test_everything_unknown_before_draw():
 def test_no_cards_known_before_draw():
     player_view = PlayerView()
     assert player_view.certain_cards == set()
+
+
+def test_mark_has_card_removes_card_from_possible_cards():
+    card = Card(Suit.BLACK, 3)
+    player_view = PlayerView()
+    player_view.mark_has_card(card)
+    assert card in player_view.certain_cards
