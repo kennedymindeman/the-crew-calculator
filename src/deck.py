bfbb9513be3964a21c4abfd9f93f104504978fb3
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-22 23:02:44 -0500

    Fix bug with card colors

--- src/deck.py
+++ src/deck.py
@@ -6,8 +6,8 @@ class Deck:
     BLACK_CARDS = frozenset(Card(Suit.BLACK, rank) for rank in range(1, 5))
     BLUE_CARDS = frozenset(Card(Suit.BLUE, rank) for rank in range(1, 10))
     GREEN_CARDS = frozenset(Card(Suit.GREEN, rank) for rank in range(1, 10))
-    PINK_CARDS = frozenset(Card(Suit.GREEN, rank) for rank in range(1, 10))
-    YELLOW_CARDS = frozenset(Card(Suit.GREEN, rank) for rank in range(1, 10))
+    PINK_CARDS = frozenset(Card(Suit.PINK, rank) for rank in range(1, 10))
+    YELLOW_CARDS = frozenset(Card(Suit.YELLOW, rank) for rank in range(1, 10))
     ALL_CARDS = BLACK_CARDS | BLUE_CARDS | GREEN_CARDS | PINK_CARDS | YELLOW_CARDS
 
     def __init__(self) -> None:
