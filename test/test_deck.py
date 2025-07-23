95e9081a4918531a68e6c790480108b8aaea2f49
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-22 21:25:16 -0500

    Add check for draw_pile initially containing all cards

--- test/test_deck.py
+++ test/test_deck.py
@@ -4,3 +4,8 @@ from src.deck import Deck
 
 def test_blue_1_in_deck():
     assert Card(Suit.BLUE, 1) in Deck.ALL_CARDS
+
+
+def test_all_cards_in_deck_stack_after_constructor():
+    deck = Deck()
+    assert set(deck.draw_pile) == Deck.ALL_CARDS
