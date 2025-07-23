d4a4e8c2fb6c47d3954c0712fdd7b7a5f569cadd
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-22 22:55:56 -0500

    Add hand member to Player

--- src/player.py
+++ src/player.py
@@ -5,6 +5,7 @@ from src.player_view import PlayerView
 
 class Player:
     def __init__(self, *, player_id: int, num_players: int) -> None:
+        self.hand = set()
         self.player_id = player_id
         self.player_views = [PlayerView() for _ in range(num_players)]
 
@@ -13,4 +14,5 @@ class Player:
         for player_view in self.player_views:
             player_view.eliminate_card(card)
         self.player_views[self.player_id].mark_has_card(card)
+        self.hand.add(card)
         return card
