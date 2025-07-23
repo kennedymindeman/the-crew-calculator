960f56493edacc6ffc576ddbd26171e9a28a714d
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-22 22:00:37 -0500

    Add test for all cards not being known before draws

--- test/test_player_view.py
+++ test/test_player_view.py
@@ -5,3 +5,8 @@ from src.player_view import PlayerView
 def test_everything_unknown_before_draw():
     player_view = PlayerView()
     assert player_view.possible_cards == Deck.ALL_CARDS
+
+
+def test_no_cards_known_before_draw():
+    player_view = PlayerView()
+    assert player_view.possible_cards == set()
